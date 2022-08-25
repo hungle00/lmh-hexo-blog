6a9755bbecff63fee9da20aab28c1ef5bc265040
hungle00 <hungle2048@gmail.com>
2022-08-25 22:48:45 +0700

    replace css by tailwind

--- themes/jedi/source/js/main.js
+++ themes/jedi/source/js/main.js
@@ -30,7 +30,7 @@
   const cbox = document.getElementById("menu-trigger");
 
   cbox.addEventListener("change", function () {
-    const area = document.querySelector(".wrapper");
+    const area = document.querySelector("#wrapper");
     this.checked
       ? area.classList.add("blurry")
       : area.classList.remove("blurry");
